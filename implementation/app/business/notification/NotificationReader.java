b19b55c2ec8616bccb509ba0fb419d95f51865a9
david.peer <david@peer.tirol>
2016-04-04 13:14:38 +0200

    Added missing files

--- implementation/app/business/notification/NotificationReader.java
+++ implementation/app/business/notification/NotificationReader.java
@@ -1,9 +1,11 @@
 package business.notification;
 
 import business.UseCases;
+import model.Notification;
 
 /**
  * Created by david on 22.03.16.
  */
 public interface NotificationReader extends UseCases {
+    Notification getNotifictionForUser(int userId);
 }
--- implementation/app/business/notification/NotificationReaderFacade.java
+++ implementation/app/business/notification/NotificationReaderFacade.java
@@ -1,7 +1,13 @@
 package business.notification;
 
+import model.Notification;
+
 /**
  * Created by david on 22.03.16.
  */
 public class NotificationReaderFacade implements NotificationReader {
+    @Override
+    public Notification getNotifictionForUser(int userId) {
+        return new Notification("Hallo", null, null, false);
+    }
 }
