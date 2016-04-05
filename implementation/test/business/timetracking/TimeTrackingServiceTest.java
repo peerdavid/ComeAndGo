36ce51e957cf52c1765d1356f9a171d3c5f8f060
david.peer <david@peer.tirol>
2016-04-05 12:43:39 +0200

    Fixed unit tests

--- implementation/test/business/timetracking/TimeTrackingServiceTest.java
+++ implementation/test/business/timetracking/TimeTrackingServiceTest.java
@@ -27,7 +27,7 @@ public class TimeTrackingServiceTest {
 
     @Before
     public void SetUp() throws Exception {
-        _testUser = new User("", "", "", "", "", true);
+        _testUser = new User("", "", "", "", "", "user@paz.at", true);
 
         _notificationSenderMock = mock(NotificationSender.class);
         _timeTrackingRepository = mock(TimeTrackingRepository.class);
