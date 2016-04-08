0e9294946aca9d391ba358f115172a24831f9367
csar8386 <teqeme05>
2016-04-08 11:22:32 +0200

    implement TimeTracking (wip)

--- implementation/app/business/timetracking/TimeTrackingServiceImpl.java
+++ implementation/app/business/timetracking/TimeTrackingServiceImpl.java
@@ -3,6 +3,7 @@ package business.timetracking;
 
 import business.notification.NotificationSender;
 import infrastructure.TimeTrackingRepository;
+import infrastructure.UserRepository;
 import javassist.NotFoundException;
 import model.Notification;
 import model.TimeTrack;
@@ -15,20 +16,23 @@ import com.google.inject.Inject;
 /**
  * Created by david on 21.03.16.
  */
-class TimeTrackingServiceImpl implements TimeTrackingService{
-
-    private TimeTrackingRepository _repository;
-    private NotificationSender _notificationSender;
+class TimeTrackingServiceImpl implements TimeTrackingService {
+    private final TimeTrackingRepository _repository;
+    private final NotificationSender _notificationSender;
+    private final UserRepository _userRepository;
 
     @Inject
-    public TimeTrackingServiceImpl(TimeTrackingRepository repository, NotificationSender notificationSender){
+    public TimeTrackingServiceImpl(TimeTrackingRepository repository, NotificationSender notificationSender, UserRepository userRepository) {
         _repository = repository;
         _notificationSender = notificationSender;
+        _userRepository = userRepository;
     }
 
 
     @Override
-    public int come() {
+    public int come(int userId) {
+        User user = _userRepository.readUser(userId);
+
         TimeTrack newTimeTrack = new TimeTrack();
         newTimeTrack.set_from(DateTime.now());
         int newId = _repository.createTimeTrack(newTimeTrack);
@@ -37,8 +41,10 @@ class TimeTrackingServiceImpl implements TimeTrackingService{
 
 
     @Override
-    public void go(int id) throws NotFoundException {
-        TimeTrack timeTrack = _repository.readTimeTrack(id);
+    public void go(int userId) throws NotFoundException {
+        User user = _userRepository.readUser(userId);
+
+        TimeTrack timeTrack = _repository.readTimeTrack(user.getId());
         timeTrack.set_to(DateTime.now());
         _repository.updateTimeTrack(timeTrack);
 
@@ -49,7 +55,9 @@ class TimeTrackingServiceImpl implements TimeTrackingService{
     }
 
     @Override
-    public List<TimeTrack> readTimeTracks(User user) {
+    public List<TimeTrack> readTimeTracks(int userId) {
+        User user = _userRepository.readUser(userId);
+
         return _repository.readTimeTracks(user);
     }
 }
--- implementation/app/controllers/TimeTrackController.java
+++ implementation/app/controllers/TimeTrackController.java
@@ -24,14 +24,18 @@ public class TimeTrackController extends UserProfileController<CommonProfile> {
 
     @RequiresAuthentication(clientName = "default")
     public Result index() throws Exception {
-        _timeTracking.come();
-        return ok(views.html.index.render(getUserProfile()));
+        CommonProfile profile = getUserProfile();
+
+        _timeTracking.come(Integer.parseInt(profile.getId()));
+
+        return ok(views.html.index.render(profile));
     }
 
     @RequiresAuthentication(clientName = "default")
     public Result come() throws Exception{
-        _timeTracking.come();
-        return ok(views.html.index.render(getUserProfile()));
+        CommonProfile profile = getUserProfile();
+        _timeTracking.come(Integer.parseInt(profile.getId()));
+        return ok(views.html.index.render(profile));
     }
 
     /*
@@ -44,7 +48,8 @@ public class TimeTrackController extends UserProfileController<CommonProfile> {
 
     @RequiresAuthentication(clientName = "default")
     public Result go() throws Exception{
-        _timeTracking.go();
+        CommonProfile profile = getUserProfile();
+        _timeTracking.go(Integer.parseInt(profile.getId()));
         return ok("Go");
     }
 }
