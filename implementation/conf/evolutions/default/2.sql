20e12c63bf2b18307b263e4f7978722e5d167899
Patrick Summerer <summerer.p@aon.at>
2016-04-11 15:31:12 +0200

    fixed 2.sql because of usermodel change

--- implementation/conf/evolutions/default/2.sql
+++ implementation/conf/evolutions/default/2.sql
@@ -1,6 +1,6 @@
 # --- !Ups
-insert into user (id, username, password, role, active, firstname, lastname, email)
-values (0, 'admin', '$2a$10$x5hq8cYHsF2UOCElx7v29.FAleENAZ//oHgii6DdAyarcBe514KtS', 'ROLE_ADMIN', true, 'admin', 'admin', 'admin@comego.at');
+insert into user (id, username, password, role, active, firstname, lastname, email, user_name_boss)
+values (0, 'admin', '$2a$10$x5hq8cYHsF2UOCElx7v29.FAleENAZ//oHgii6DdAyarcBe514KtS', 'ROLE_ADMIN', true, 'admin', 'admin', 'admin@comego.at', 'testboss');
 
 
 # --- !Downs
