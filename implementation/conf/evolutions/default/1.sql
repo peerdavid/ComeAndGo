62db4de6b86dd3cdc75693452f486a5d736b00c5
Martin Brunner <csar8463@student.uibk.ac.at>
2016-04-11 12:19:49 +0200

    Login View

--- implementation/conf/evolutions/default/1.sql
+++ implementation/conf/evolutions/default/1.sql
@@ -3,6 +3,22 @@
 
 # --- !Ups
 
+create table break (
+  id                            integer auto_increment not null,
+  time_track_id                 integer not null,
+  start                         datetime,
+  end                           datetime,
+  constraint pk_break primary key (id)
+);
+
+create table time_track (
+  id                            integer auto_increment not null,
+  _user_id                      integer not null,
+  start                         datetime,
+  end                           datetime,
+  constraint pk_time_track primary key (id)
+);
+
 create table user (
   id                            integer not null,
   username                      varchar(255),
@@ -16,8 +32,24 @@ create table user (
 );
 create sequence user_seq;
 
+alter table break add constraint fk_break_time_track_id foreign key (time_track_id) references time_track (id) on delete restrict on update restrict;
+create index ix_break_time_track_id on break (time_track_id);
+
+alter table time_track add constraint fk_time_track__user_id foreign key (_user_id) references user (id) on delete restrict on update restrict;
+create index ix_time_track__user_id on time_track (_user_id);
+
 
 # --- !Downs
+
+alter table break drop constraint if exists fk_break_time_track_id;
+drop index if exists ix_break_time_track_id;
+
+alter table time_track drop constraint if exists fk_time_track__user_id;
+drop index if exists ix_time_track__user_id;
+
+drop table if exists break;
+
+drop table if exists time_track;
 
 drop table if exists user;
 drop sequence if exists user_seq;
