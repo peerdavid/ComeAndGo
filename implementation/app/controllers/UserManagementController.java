39eb8cdfd8296d8a9b6e412fa58d22469a5dc7c7
Leonhard <leonhard.haas@student.uibk.ac.at>
2016-04-17 21:51:01 +0200

    removeuser in edituser view (wip) ; minor fixes on index page

--- implementation/app/controllers/UserManagementController.java
+++ implementation/app/controllers/UserManagementController.java
@@ -17,21 +17,21 @@ import static play.mvc.Results.ok;
 /**
  * Created by Leonhard on 13.04.2016.
  */
-public class UserManagementController extends UserProfileController{
+public class UserManagementController extends UserProfileController {
 
     private UserManagement _userManagement;
     public static final Form<User> FORM = Form.form(User.class);
 
     @Inject
-    public UserManagementController(UserManagement userManagement){
-        _userManagement=userManagement;
+    public UserManagementController(UserManagement userManagement) {
+        _userManagement = userManagement;
     }
 
     @RequiresAuthentication(clientName = "default", authorizerName = "admin")
-    public Result indexEditUser() throws Exception{
+    public Result indexEditUser() throws Exception {
         CommonProfile profile = getUserProfile();
 
-        return ok(views.html.edituser.render(profile,_userManagement.getAllUsers()));
+        return ok(views.html.edituser.render(profile, _userManagement.getAllUsers()));
     }
 
 
@@ -39,7 +39,7 @@ public class UserManagementController extends UserProfileController{
     Username remains unchangable because the authenticator
      */
     @RequiresAuthentication(clientName = "default", authorizerName = "admin")
-    public Result editUser() throws Exception{
+    public Result editUser() throws Exception {
         CommonProfile profile = getUserProfile();
 
         List<User> userList = _userManagement.getAllUsers();
@@ -56,31 +56,44 @@ public class UserManagementController extends UserProfileController{
 
         User changingUser = null;
 
-        for(User u : userList){
-            if(u.getId() == userId){
+        for (User u : userList) {
+            if (u.getId() == userId) {
                 changingUser = u;
                 break;
             }
         }
-       // should we edit userName?
-        if(userName != null && !userName.equals("")){
+        // should we edit userName?
+        if (userName != null && !userName.equals("")) {
             changingUser.setUserName(userName);
         }
-        if(firstName != null && !firstName.equals("")){
+        if (firstName != null && !firstName.equals("")) {
             changingUser.setFirstName(firstName);
         }
-        if(lastName != null && !lastName.equals("")){
+        if (lastName != null && !lastName.equals("")) {
             changingUser.setLastName(lastName);
         }
-        if(email != null && !email.equals("")){
+        if (email != null && !email.equals("")) {
             changingUser.setEmail(email);
         }
-        if(password != null && repeatPassword != null && (!password.isEmpty())&&(!repeatPassword.isEmpty())&&password.equals(repeatPassword)){
+        if (password != null && repeatPassword != null && (!password.isEmpty()) && (!repeatPassword.isEmpty()) && password.equals(repeatPassword)) {
             changingUser.setPassword(password);
         }
 
-        _userManagement.changeUserData(changingUser.getUserName(),changingUser);
+        _userManagement.changeUserData(changingUser.getUserName(), changingUser);
 
-        return ok(views.html.edituser.render(profile,_userManagement.getAllUsers()));
+        return ok(views.html.edituser.render(profile, _userManagement.getAllUsers()));
+    }
+
+    @RequiresAuthentication(clientName = "default", authorizerName = "admin")
+    public Result deleteUser(String userName) throws Exception {
+        CommonProfile profile = getUserProfile();
+
+        if (userName.isEmpty()) {
+            throw new Exception("Empty Username");
+        }
+
+        _userManagement.deleteUser(userName);
+
+        return redirect(routes.UserManagementController.indexEditUser());
     }
 }
