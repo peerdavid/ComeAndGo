3a83fc02aacc7da1d06c77924d8d2ec841fcf26a
csar8386 <teqeme05>
2016-04-18 15:04:56 +0200

    add edit timetracks (wip)

--- implementation/public/javascripts/comeandgo.js
+++ implementation/public/javascripts/comeandgo.js
@@ -2,14 +2,28 @@
  * Created by csaq5996 on 4/11/16.
  */
 
+$(function() {
+
+});
+
 $( document ).ready(function() {
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
     $(".button-collapse").sideNav();
     $('select').material_select();
     $('.modal-trigger').leanModal();
+
+    // initialize datepicker
+    $('.datepicker').pickadate({
+        selectMonths: true,
+        selectYears: 15,
+        // translation strings
+        monthsFull: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        weekdaysFull: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        weekdaysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        showMonthsShort: true,
+        showWeekdaysFull: false,
+        format: 'dd.mm.yyyy'
+    });
 });
 
 $('.edit-user-switch').click(function () {
