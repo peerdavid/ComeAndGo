559a9cebe6e4cbf6d4e7f2c5507958d7a55344e5
Leonhard <leonhard.haas@student.uibk.ac.at>
2016-04-18 15:05:52 +0200

    added timepicki for timepicker

--- implementation/public/javascripts/comeandgo.js
+++ implementation/public/javascripts/comeandgo.js
@@ -10,6 +10,14 @@ $( document ).ready(function() {
     $(".button-collapse").sideNav();
     $('select').material_select();
     $('.modal-trigger').leanModal();
+    $('#timepicker').timepicki({
+        show_meridian:false,
+        min_hour_value:0,
+        max_hour_value:23,
+        overflow_minutes:true,
+        increase_direction:'up',
+        disable_keyboard_mobile: true
+    });
 });
 
 $('.edit-user-switch').click(function () {
