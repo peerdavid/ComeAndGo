aa280556e0ff940f9386286eed4c260118bb7d89
csar7248 <stefan.haberl@student.uibk.ac.at>
2016-04-18 10:40:05 +0200

    added methods to edit/delete/update timeTrack & breaks

--- implementation/app/business/timetracking/TimeTracking.java
+++ implementation/app/business/timetracking/TimeTracking.java
@@ -1,6 +1,7 @@
 package business.timetracking;
 
 import business.UseCases;
+import models.Break;
 import models.TimeTrack;
 import org.joda.time.DateTime;
 
@@ -23,4 +24,30 @@ public interface TimeTracking extends UseCases {
     List<TimeTrack> readTimeTracks(int userId);
 
     List<TimeTrack> readTimeTracks(int userId, DateTime from, DateTime to);
+
+    /**
+     * adds the new timeTrack to repository
+     * @param timeTrack
+     */
+    void addTimeTrack(TimeTrack timeTrack);
+
+    /**
+     * delete timeTrack
+     * @param timeTrack
+     */
+    void deleteTimeTrack(TimeTrack timeTrack);
+
+    void updateTimeTrack(TimeTrack timeTrack);
+
+    /**
+     * add breakToInsert which belongs to the specified timeTrack
+     * @param timeTrack
+     * @param breakToInsert
+     */
+    void addBreak(TimeTrack timeTrack, Break breakToInsert);
+
+    void deleteBreak(Break breakToDelete);
+
+    void updateBreak(Break breakToUpdate);
+    
 }
