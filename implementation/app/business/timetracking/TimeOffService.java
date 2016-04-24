08fc3472ea361977d00e3fb392146f029b864367
Patrick Summerer <summerer.p@aon.at>
2016-04-24 18:14:23 +0200

    added TimeOff model (SpecialDay)

---
+++ implementation/app/business/timetracking/TimeOffService.java
@@ -0,0 +1,8 @@
+package business.timetracking;
+
+/**
+ * Created by paz on 24.04.16.
+ */
+public interface TimeOffService {
+
+}
---
+++ implementation/app/business/timetracking/TimeOffState.java
@@ -0,0 +1,11 @@
+package business.timetracking;
+
+/**
+ * Created by paz on 24.04.16.
+ */
+public enum TimeOffState {
+    NEW,
+    REQUEST_SENT,
+    REQUEST_ACCEPTED,
+    REQUEST_REJECTED
+}
