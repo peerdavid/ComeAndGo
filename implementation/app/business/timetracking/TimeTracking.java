10d71823055dc6f35980b71a270700eaf8046dd6
Patrick Summerer <summerer.p@aon.at>
2016-04-28 13:02:41 +0200

    added Parental leave and educational leave to TimeTracking interface

--- implementation/app/business/timetracking/TimeTracking.java
+++ implementation/app/business/timetracking/TimeTracking.java
@@ -37,11 +37,23 @@ public interface TimeTracking extends UseCases {
 
     void takeBusinessTrip(int userId, DateTime from, DateTime to, String comment) throws Exception;
 
+    void takeParentalLeave(int userId, DateTime from, DateTime to, String comment) throws Exception;
+
     void  requestHoliday(int userId, DateTime from, DateTime to, String comment) throws Exception;
 
     void  requestSpecialHoliday(int userId, DateTime from, DateTime to, String comment) throws Exception;
 
+    void  requestEducationalLeave(int userId, DateTime from, DateTime to, String comment) throws Exception;
+
     void  acceptHoliday(int timeOffId, int bossId) throws Exception;
 
     void  rejectHoliday(int timeOffId, int bossId) throws Exception;
+
+    void  acceptSpecialHoliday(int timeOffId, int bossId) throws Exception;
+
+    void  rejectSpecialHoliday(int timeOffId, int bossId) throws Exception;
+
+    void  acceptEducationalLeave(int timeOffId, int bossId) throws Exception;
+
+    void  rejectEducationalLeave(int timeOffId, int bossId) throws Exception;
 }
