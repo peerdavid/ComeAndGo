25af02e72b7899dd0fe5badba788405235920672
csar8386 <teqeme05>
2016-04-29 09:30:31 +0200

    add new methods for parsing a date string

--- implementation/app/utils/DateTimeUtils.java
+++ implementation/app/utils/DateTimeUtils.java
@@ -9,6 +9,8 @@ import java.io.PrintWriter;
  */
 public class DateTimeUtils {
 
+    private final static DateTime EMPTY_DATE = new DateTime(0, 0, 0, 0, 0);
+
     public static String dateTimeToTime(DateTime dateTime) {
         int hours = dateTime.getHourOfDay();
         int minutes = dateTime.getMinuteOfHour();
@@ -27,5 +29,45 @@ public class DateTimeUtils {
         String result = String.format("%02d.%02d.%4d", day, month, year);
 
         return result;
+    }
+
+    public static DateTime stringToTime(String time) {
+        return stringToTime(time, EMPTY_DATE);
+    }
+
+    public static DateTime stringToTime(String time, DateTime date) {
+        String[] parts = time.split(":");
+
+        return new DateTime(
+            date.getYear(),
+            date.getMonthOfYear(),
+            date.getDayOfMonth(),
+            Integer.parseInt(parts[0]),
+            Integer.parseInt(parts[1])
+        );
+    }
+
+    public static DateTime stringToDateTime(String date) {
+        return stringToDateTime(date, 0, 0);
+    }
+
+    public static DateTime stringToDateTime(String date, int hours, int minutes) {
+        String[] parts = date.split("\\.");
+        return new DateTime(
+            Integer.parseInt(parts[2]),
+            Integer.parseInt(parts[1]),
+            Integer.parseInt(parts[0]),
+            hours, minutes
+        );
+    }
+
+    public static DateTime mergeDateAndTime(DateTime date, DateTime time) {
+        return new DateTime(
+            date.getYear(),
+            date.getMonthOfYear(),
+            date.getDayOfMonth(),
+            time.getHourOfDay(),
+            time.getMinuteOfHour()
+        );
     }
 }
