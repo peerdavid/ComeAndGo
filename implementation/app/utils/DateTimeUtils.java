42ebaf7fdd364b5570fd695c182acc0679d1c252
csar8386 <teqeme05>
2016-04-29 08:40:55 +0200

    minor code styling fixes in DateTimeUtils

--- implementation/app/utils/DateTimeUtils.java
+++ implementation/app/utils/DateTimeUtils.java
@@ -9,22 +9,22 @@ import java.io.PrintWriter;
  */
 public class DateTimeUtils {
 
-    public static String dateTimeToTime(DateTime dateTime){
+    public static String dateTimeToTime(DateTime dateTime) {
         int hours = dateTime.getHourOfDay();
         int minutes = dateTime.getMinuteOfHour();
 
-        String result = String.format("%02d:%02d",hours,minutes);
+        String result = String.format("%02d:%02d", hours, minutes);
 
         return result;
 
     }
 
-    public static String dateTimeToDate(DateTime dateTime){
+    public static String dateTimeToDate(DateTime dateTime) {
         int day = dateTime.getDayOfMonth();
         int month = dateTime.getMonthOfYear();
         int year = dateTime.getYear();
 
-        String result = String.format("%02d.%02d.%4d",day,month,year);
+        String result = String.format("%02d.%02d.%4d", day, month, year);
 
         return result;
     }
