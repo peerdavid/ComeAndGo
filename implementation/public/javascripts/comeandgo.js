d6b91949ce1e4382ae2048d9f24ecf881c71e665
csar8386 <teqeme05>
2016-04-29 08:26:55 +0200

    fix bug where date picker is picking wrong year
    (19XX instead of 20XX)
    always pick current value of input field

--- implementation/public/javascripts/comeandgo.js
+++ implementation/public/javascripts/comeandgo.js
@@ -12,11 +12,26 @@ $( document ).ready(function() {
         ampmclickable: false,
         container: 'body'
     });
+});
+
+$('.enable-editing-switch').click(function () {
+    $('li.active form input').prop('disabled', !this.checked);
+    $('li.active form button.ghost').css('display', (this.checked ? 'inline' : 'none'));
+    //$('form select').attr('disabled', 'disabled');
+});
+
+/* fix for datepicker bug showing 19XX instead of 20XX */
+$('.datepicker').focus(function () {
+    var value = this.value.split('.');
+    if(value.length < 3) return;
 
     // initialize datepicker
     $('.datepicker').pickadate({
         selectMonths: true,
-        selectYears: 300,
+        year: parseInt(value[2]) + 100,
+        month: parseInt(value[1]),
+        date: parseInt(value[0]),
+        selectYears: 10,
         // translation strings
         monthsFull: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
@@ -27,10 +42,4 @@ $( document ).ready(function() {
         format: 'dd.mm.yyyy',
         container: 'body'
     });
-});
-
-$('.enable-editing-switch').click(function () {
-    $('li.active form input').prop('disabled', !this.checked);
-    $('li.active form button.ghost').css('display', (this.checked ? 'inline' : 'none'));
-    //$('form select').attr('disabled', 'disabled');
 });
