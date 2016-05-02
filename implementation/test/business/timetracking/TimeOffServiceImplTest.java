b96143f964e864e365785d200e864f8d7577b24a
peerdavid <david@peer.tirol>
2016-05-02 11:39:03 +0200

    Added unit tests and validation for holiday reject

--- implementation/test/business/timetracking/TimeOffServiceImplTest.java
+++ implementation/test/business/timetracking/TimeOffServiceImplTest.java
@@ -28,8 +28,10 @@ public class TimeOffServiceImplTest {
     private NotificationSender _notificationSenderMock;
     private InternalUserManagement _internalUserManagementMock;
     private TimeOffService _testee;
-    private User _testUserMock;
-    private int _testUserId = 7;
+    private User _testEmployeeMock;
+    private int _testEmployeeId = 7;
+    private User _testBossMock;
+    private int _testBossId = 20;
     private TimeOff _timeOffFutureMock;
     private int _timeOffFutureId = 17;
     private TimeOff _timeOffPastMock;
@@ -42,20 +44,25 @@ public class TimeOffServiceImplTest {
         _timeOffValidationMock = mock(TimeOffValidation.class);
         _notificationSenderMock = mock(NotificationSender.class);
 
-        _testUserMock = mock(User.class);
-        when(_testUserMock.getId()).thenReturn(_testUserId);
+        _testBossMock = mock(User.class);
+        when(_testBossMock.getId()).thenReturn(_testBossId);
+
+        _testEmployeeMock = mock(User.class);
+        when(_testEmployeeMock.getId()).thenReturn(_testEmployeeId);
+        when(_testEmployeeMock.get_boss()).thenReturn(_testBossMock);
         _internalUserManagementMock = mock(InternalUserManagement.class);
-        when(_internalUserManagementMock.readUser(_testUserId)).thenReturn(_testUserMock);
+        when(_internalUserManagementMock.readUser(_testEmployeeId)).thenReturn(_testEmployeeMock);
+        when(_internalUserManagementMock.readUser(_testBossId)).thenReturn(_testBossMock);
 
         _timeOffFutureMock = mock(TimeOff.class);
         when(_timeOffFutureMock.getId()).thenReturn(_timeOffFutureId);
-        when(_timeOffFutureMock.getUser()).thenReturn(_testUserMock);
+        when(_timeOffFutureMock.getUser()).thenReturn(_testEmployeeMock);
         when(_timeOffFutureMock.getFrom()).thenReturn(DateTime.now().plusDays(1));
         when(_timeOffFutureMock.getTo()).thenReturn(DateTime.now().plusDays(2));
 
         _timeOffPastMock = mock(TimeOff.class);
         when(_timeOffPastMock.getId()).thenReturn(_timeOffPastId);
-        when(_timeOffPastMock.getUser()).thenReturn(_testUserMock);
+        when(_timeOffPastMock.getUser()).thenReturn(_testEmployeeMock);
         when(_timeOffPastMock.getFrom()).thenReturn(DateTime.now().minusDays(2));
         when(_timeOffPastMock.getTo()).thenReturn(DateTime.now().minusDays(1));
 
@@ -69,13 +76,13 @@ public class TimeOffServiceImplTest {
 
     @Test
     public void deleteTimeTrack_FutureTimeTrack_ShouldSucceed() throws Exception{
-        _testee.deleteTimeTrack(_testUserId, _timeOffFutureId);
+        _testee.deleteTimeTrack(_testEmployeeId, _timeOffFutureId);
     }
 
 
     @Test
     public void deleteTimeTrack_ValidInputData_ShouldSendMessageToBoss() throws Exception{
-        _testee.deleteTimeTrack(_testUserId, _timeOffFutureId);
+        _testee.deleteTimeTrack(_testEmployeeId, _timeOffFutureId);
 
         Mockito.verify(_notificationSenderMock, times(1)).sendNotification(any(Notification.class)); // Check if the function really called our repository
     }
@@ -83,12 +90,31 @@ public class TimeOffServiceImplTest {
 
     @Test(expected = NotAuthorizedException.class)
     public void deleteTimeTrack_UserWantsToDeleteTimeOffOfAnotherUser_ShouldFail() throws Exception{
-        _testee.deleteTimeTrack(_testUserId + 1, _timeOffFutureId);
+        _testee.deleteTimeTrack(_testEmployeeId + 1, _timeOffFutureId);
     }
 
 
     @Test(expected = UserException.class)
     public void deleteTimeTrack_UserWantsToDeleteOldTimeOff_ShouldFail() throws Exception{
-        _testee.deleteTimeTrack(_testUserId, _timeOffPastId);
+        _testee.deleteTimeTrack(_testEmployeeId, _timeOffPastId);
+    }
+
+
+    @Test
+    public void rejectHoliday_BossIsBossOfUser_ShouldSucceed() throws Exception{
+        _testee.rejectHoliday(_timeOffFutureId, _testBossId);
+    }
+
+
+    @Test
+    public void rejectHoliday_BossIsBossOfUser_ShouldSendNotification() throws Exception{
+        _testee.rejectHoliday(_timeOffFutureId, _testBossId);
+        Mockito.verify(_notificationSenderMock, times(1)).sendNotification(any(Notification.class)); // Check if the function really called our repository
+    }
+
+
+    @Test(expected = NotAuthorizedException.class)
+    public void rejectHoliday_BossIsNotBossOfUser_ShouldFail() throws Exception{
+        _testee.rejectHoliday(_timeOffFutureId, _testBossId + 1);
     }
 }
