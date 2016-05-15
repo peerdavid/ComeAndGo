ec6c985eb1054054357b9b99d3b187a0d2e36c6e
Patrick Summerer <summerer.p@aon.at>
2016-05-15 16:10:09 +0200

    added unit tests for collective agreement

--- implementation/test/business/reporting/CollectiveAgreementImplTest.java
+++ implementation/test/business/reporting/CollectiveAgreementImplTest.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static java.util.Collections.EMPTY_LIST;
 import static org.mockito.Mockito.mock;
 
 /**
@@ -28,18 +27,17 @@ public class CollectiveAgreementImplTest {
 
     User _testUser;
     CollectiveAgreement _testee;
-    List<TimeOff> _holiday;
+    List<TimeOff> _timeoffs;
+    List<TimeTrack> _timetracks;
 
     @Before
     public void SetUp() throws Exception {
         _testUser = new User("testUser", "test1234", SecurityRole.ROLE_USER, "Klaus", "Kleber", "klaus@kleber.at", true, null, 8.0);
-        _testUser.setEntryDate(new DateTime(2016, 01, 10, 0, 0));
+        _testUser.setEntryDate(new DateTime(2016, 1, 10, 0, 0));
         _testUser.setHolidays(25);
         _testee = new CollectiveAgreementImpl();
-
-        TimeOff holiday21days = new TimeOff(_testUser, new DateTime(2016, 2, 1, 0, 0), new DateTime(2016, 2, 20, 0, 0), TimeOffType.HOLIDAY, RequestState.REQUEST_ACCEPTED, "");
-        _holiday = new ArrayList<>();
-        _holiday.add(holiday21days);
+        _timeoffs = new ArrayList<>();
+        _timetracks = new ArrayList<>();
     }
 
     @Test
@@ -59,7 +57,10 @@ public class CollectiveAgreementImplTest {
 
     @Test
     public void createUserReport_WithHoliday_ShouldSucceed() throws Exception {
-        ReportEntry actual = _testee.createUserReport(_testUser, Collections.EMPTY_LIST, _holiday, Collections.EMPTY_LIST);
+        TimeOff holiday21days = new TimeOff(_testUser, new DateTime(2016, 2, 1, 0, 0), new DateTime(2016, 2, 20, 0, 0), TimeOffType.HOLIDAY, RequestState.REQUEST_ACCEPTED, "");
+        _timeoffs.add(holiday21days);
+
+        ReportEntry actual = _testee.createUserReport(_testUser, Collections.EMPTY_LIST, _timeoffs, Collections.EMPTY_LIST);
 
         Assert.assertEquals(0, actual.getNumOfSickDays());
         Assert.assertEquals(10, actual.getNumOfUnusedHolidays());
@@ -69,8 +70,31 @@ public class CollectiveAgreementImplTest {
 
         int expectedWorkMinutesShould = (int)_testUser.getHoursPerDay() * 60 * (
                 DateTimeUtils.getWorkdaysOfTimeInterval(_testUser.getEntryDate(), DateTime.now())
-                - DateTimeUtils.getWorkdaysOfTimeInterval(_holiday.get(0).getFrom(), _holiday.get(0).getTo()));
+                - DateTimeUtils.getWorkdaysOfTimeInterval(_timeoffs.get(0).getFrom(), _timeoffs.get(0).getTo()));
         Assert.assertEquals(expectedWorkMinutesShould, actual.getWorkMinutesShould());
+    }
+
+    @Test
+    public void createUserReport_With1DayWorkingWithoutBreaks_ShouldSucceed() throws Exception {
+        DateTime date1 = new DateTime(2016, 1, 12, 7, 55);
+        DateTime date2 = new DateTime(2016, 1, 12, 16, 57);
+
+        TimeTrack work1dayWithoutBreaks = new TimeTrack(_testUser, date1, date2, Collections.EMPTY_LIST);
+        _timetracks.add(work1dayWithoutBreaks);
+
+        ReportEntry actual = _testee.createUserReport(_testUser, _timetracks, _timeoffs, Collections.EMPTY_LIST);
+
+        Assert.assertEquals(0, actual.getNumOfSickDays());
+        Assert.assertEquals(25, actual.getNumOfUnusedHolidays());
+        Assert.assertEquals(0, actual.getNumOfUsedHolidays());
+        Assert.assertEquals(0, actual.getBreakMinutes());
+
+        int expectedWorkMinutesIs = (date2.getMinuteOfDay() - date1.getMinuteOfDay());
+        Assert.assertEquals(expectedWorkMinutesIs, actual.getWorkMinutesIs());
+
+        int expectedWorkMinutesShould = (int)_testUser.getHoursPerDay() * 60 * DateTimeUtils.getWorkdaysOfTimeInterval(_testUser.getEntryDate(), DateTime.now());
+        Assert.assertEquals(expectedWorkMinutesShould, actual.getWorkMinutesShould());
+
     }
 
 }
