61eb3e9f20655415593c2ffb0b63f0c3cfd71446
Patrick Summerer <summerer.p@aon.at>
2016-05-30 19:11:36 +0200

    changed password of all test-users to test1234

--- implementation/conf/evolutions/default/2.sql
+++ implementation/conf/evolutions/default/2.sql
@@ -1,6 +1,6 @@
 # --- !Ups
 
-insert into user values (1, 'sebastian', '$2a$10$x5hq8cYHsF2UOCElx7v29.FAleENAZ//oHgii6DdAyarcBe514KtS', 'ROLE_ADMIN', true, 'Sebastian', 'Waldhart', 'admin@comego.at', 1, 8.00, 25, '2016-05-01');
+insert into user values (1, 'sebastian', '$2a$06$F9jjMmhp7eLERO4fosg3N.I7PDSv5pRKElhYKQJ4AfMx6s6e2vlku', 'ROLE_ADMIN', true, 'Sebastian', 'Waldhart', 'admin@comego.at', 1, 8.00, 25, '2016-05-01');
 
 insert into notification VALUES
 (1, 0, 'Welcome to Come & Go.', 1, 1, false, '2016-04-23 08:10:09', 0),
